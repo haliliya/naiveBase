41a2c106bee7f3abeecbfc27f4e46c0e4bc999e9
shaharar <shaharar@post.bgu.ac.il>
2019-08-06 04:20:16 +0300

    gui, validation

--- Main.py
+++ Main.py
@@ -3,11 +3,12 @@ from Classifier import Classifier
 import pandas as pd
 from Tkinter import *
 import tkFileDialog
+import tkMessageBox
+import os
 
-# TODO check if file exists and format is valid
 # The following function reads structure.txt file in the specified path
 def read_structure_file(path):
-    file = open(path + "\\Structure.txt", "r")
+    file = open(path.get() + "\\Structure.txt", "r")
     for line in file:
         description = (line.replace("@ATTRIBUTE ", "").replace("\n", "")).split(" ", 1)
         create_feature(description)
@@ -24,26 +25,104 @@ def create_feature(feature_description):
     # append feature to features_list
     features_list.append(feature)
 
-# TODO check if file exists and format is valid
 # The following function reads a csv file in the specified path
 def read_csv(path):
-    df = pd.read_csv(path)
+    try:
+        df = pd.read_csv(path)
+    except:
+        df = pd.DataFrame()
     return df
 
 def browse_button():
     # Allow user to select a directory and store it in global var
     # called folder_path
     global folder_path
-    filename = tkFileDialog.askdirectory()
-    folder_path.set(filename)
+    folderName = tkFileDialog.askdirectory()
+    folder_path.set(folderName)
+    validateBeforeBuild()
+
+def validateBeforeBuild():
+    validFolder = validateFolderPath()
+    validBins = validateBins()
+    if (validFolder & validBins):
+        build_button.config(state="normal")
+    else:
+        build_button.config(state="disabled")
+
+def validateBins():
+    # check validation of bins
+    isValid =True
+    if (not discretization_bins_entry.get()):
+        tkMessageBox.showerror("Naive Bayes Classifier", "Please enter number of bins")
+        isValid = False
+    try:
+        if (int(discretization_bins_entry.get()) <= 0):
+            tkMessageBox.showerror("Naive Bayes Classifier", "Please enter number of bins greater than 0")
+            isValid = False
+    except:
+        tkMessageBox.showerror("Naive Bayes Classifier", "Please enter an integer number")
+        isValid = False
+
+    return isValid
+
+def validateFolderPath():
+    # check validation of chosen folder path
+    isValid = True
+    if (not os.path.isdir(folder_path.get())):
+        tkMessageBox.showerror("Naive Bayes Classifier", "You should choose a folder path")
+        isValid = False
+
+    # validate files in folder path
+    global trainFile, testFile
+    structureFile = folder_path.get() + "\\Structure.txt"
+    trainFile = folder_path.get() + "\\train.csv"
+    testFile = folder_path.get() + "\\test.csv"
+
+    # Structure.txt file
+    if (not os.path.exists(structureFile)):
+        tkMessageBox.showerror("Naive Bayes Classifier", "'Structure.txt' file does not exist in the chosen folder")
+        isValid = False
+    else:
+        if (os.stat(structureFile).st_size == 0):
+            tkMessageBox.showerror("Naive Bayes Classifier", "'Structure.txt' file is empty")
+            isValid = False
+
+    # train.csv file
+    if (not os.path.exists(trainFile)):
+        tkMessageBox.showerror("Naive Bayes Classifier", "'train.csv' file does not exist in the chosen folder")
+        isValid = False
+
+    # test.csv file
+    if (not os.path.exists(testFile)):
+        tkMessageBox.showerror("Naive Bayes Classifier", "'test.csv' file does not exist in the chosen folder")
+        isValid = False
+
+    return isValid
 
 def build():
-    print "build"
-    '''TODO complete function'''
+    read_structure_file(folder_path)
+    global train_set
+    train_set = read_csv(trainFile)
+    if (train_set.empty):
+        tkMessageBox.showerror("Naive Bayes Classifier", "'train.csv' file is empty")
+        return
+
+    global classifier
+    classifier = Classifier(features_list, train_set)
+    numOfBins = (int)(discretization_bins_entry.get())
+    classifier.pre_process_data(numOfBins)
+    tkMessageBox.showinfo("Naive Bayes Classifier", "Building classifier using train-set is done!")
 
 def classify():
-    '''TODO complete function'''
-    print "classify"
+    global test_set
+    test_set = read_csv(testFile)
+    if (test_set.empty):
+        tkMessageBox.showerror("Naive Bayes Classifier", "'test.csv' file is empty")
+        return
+
+    classifier.classify(test_set, folder_path.get() + "/output.txt")
+    tkMessageBox.showinfo("Naive Bayes Classifier","Classifying test-set is done! You can find the results in the chosen folder.")
+    root.destroy()
 
 features_list = []
 '''
@@ -57,9 +136,8 @@ test_set = read_csv("C:\\Users\\yardenhalili\\PycharmProjects\\naiveBayes\\test.
 classifier.classify(test_set, "C:\\Users\\yardenhalili\\PycharmProjects\\naiveBayes\\output.txt")
 '''
 
-
 root = Tk()
-root.title("naive bayes classifier")
+root.title("Naive Bayes Classifier")
 '''
 directory_path_label = Label(root, text="Directory Path")
 directory_path_label.grid(row=0, column=0, sticky=W)
@@ -72,19 +150,21 @@ discretization_bins_label = Label(root, text="Discretization Bins:")
 directory_path_entry = Entry(root, textvariable=folder_path)
 discretization_bins_entry = Entry(root)
 
-directory_path_entry.grid(row=0, column=1, sticky=W)
-discretization_bins_entry.grid(row=1, column=1, sticky=W)
+directory_path_entry.grid(row=0, column=1, sticky=W, padx=5, pady=5)
+discretization_bins_entry.grid(row=1, column=1, sticky=W, padx=5, pady=5)
 
-directory_path_label.grid(row=0, column=0, sticky=W)
-discretization_bins_label.grid(row=1, column=0, sticky=W)
+directory_path_label.grid(row=0, column=0, sticky=W, padx=5, pady=5)
+discretization_bins_label.grid(row=1, column=0, sticky=W, padx=5, pady=5)
 
 browse_button = Button(text="Browse", command=browse_button)
-browse_button.grid(row=0, column=3)
+browse_button.grid(row=0, column=8, padx=5, pady=5)
 
-build_button = Button(text="Build", command=build)
-build_button.grid(row=5, column=1)
+build_button = Button(text="Build", command=build, state="disabled")
+build_button.grid(row=5, column=1, padx=5, pady=5)
 
 classify_button = Button(text="Classify", command=classify)
-classify_button.grid(row=10,column=1)
+classify_button.grid(row=10,column=1, padx=5, pady=5)
 
+root.geometry("350x150")
+root.resizable(width=True, height=True)
 root.mainloop()
